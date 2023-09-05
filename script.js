f84e930c82422cdc77b9caa0a9181919b88b9a45
Blacktszzz <121826821+Blacktszzz@users.noreply.github.com>
2023-09-05 11:16:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let musica = document.querySelector("audio");
+
+document.querySelector("botão play").addEventListener("click",tocarMusica);
+
+function tocarMusica(){
+    musica.play
+}
+function atualizarbarra(){
+    let barra = document.querySelector ("progress");
+    barra.style.width = 
+}
